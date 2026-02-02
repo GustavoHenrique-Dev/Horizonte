6eac3c99fb55ad6e59fa14f75065535d13f10f5b
Gustavo <gustavohps654@gmail.com>
2026-02-02 17:41:48 -0300

    .

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,35 @@
 import Style from "./Header.module.css"
+import { NavLink } from "react-router-dom"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className={Style.header}>
+      <div className={Style.headerLogo}>
+        <h1>Horizonte</h1>
+      </div>
+
+      <div className={Style.navBar}>
+        <div className={Style.navBar-Auth}>
+          <button>Entrar</button>
+          <button>Cadastrar</button>
+        </div>
+        <div className={Style.navBar-Links}>
+          <NavLink to="/">Início</NavLink>
+          <NavLink to="/properties">Imóveis</NavLink>
+          <NavLink to="/about">Sobre</NavLink>
+          <NavLink to="/contact">Contato</NavLink>
+        </div>
+        <div className={Style.navBar-logOut}>
+          <button>Sair</button>
+          <button>Configurações</button>
+        </div>
+      </div>
+
+      <div className={Style.headerAuth}>
+        <button>Entrar</button>
+        <button>Cadastar</button>
+      </div>
+    </header>
   )
 }
 
